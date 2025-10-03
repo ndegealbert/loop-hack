a0576250e23802c7d8c81414fd93b09887c5e3fc
Albert Ndege <Albert.Ndege@Admins-MacBook-Pro-10.local>
2025-10-04 02:27:05 +0300

    debug

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,5 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: 'export',          // <-- this replaces "next export"
-    images: { unoptimized: true } // needed for static export on Netlify
-  };
-  module.exports = nextConfig;
-  
+const nextConfig = {};
+
+export default nextConfig;
+
